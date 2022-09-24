e7d5fda88e845a27b90859cfe60f33ebd7a5c48a
Viviana <vivitr5195@gmail.com>
2022-09-24 21:25:33 +0200

    init repo

---
+++ src/views/MyView.vue
@@ -0,0 +1,16 @@
+<script>
+    export default{};
+</script>
+
+<template>
+    <div>
+      <h1>Init</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  h1{
+     color: rgb(74, 14, 216);
+  }
+
+</style>
