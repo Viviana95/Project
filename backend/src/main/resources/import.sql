74e22bf370b107e8de479ecd69a8e7ade723d7d2
Viviana <vivitr5195@gmail.com>
2022-10-23 14:00:36 +0200

    crud back ok

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,6 +2,7 @@
 
 ALTER TABLE products AUTO_INCREMENT = 1;
 
-INSERT INTO products(title, description, price, image) values ("tarta de manzana", "muy rica", "usd", "url");
-INSERT INTO products(title, description, price, image) values ("cupcakes", "hay que repetir", "usd", "url");
-INSERT INTO products(title, description, price, image) values ("cookies", "me encanta", "usd", "url");
+INSERT INTO products(title, description, price, image) values ("Tarta de limón", "Una cuajada de limón sin azúcar es el relleno para este pastel dulce y agrio. La corteza utiliza harina de almendras y coco para un sabor abundante.", "10.00 EUR", "https://cdn.cheapism.com/images/101017_diabetic_dessert_recipes_slide_45_fs.max-784x410.jpg");
+INSERT INTO products(title, description, price, image) values ("Polos de fresa y yogurt", "Estos helados de sorpresa congelada, son bajas en grasas y carbohidratos, mientras que se mantienen gruesos y cremosas, gracias al yogur griego.", "1.00 EUR","https://cdn.cheapism.com/images/101017_diabetic_dessert_recipes_slide_11_fs.max-784x410.jpg");
+INSERT INTO products(title, description, price, image) values ("Cookies", "Esta adaptación de pastel helado utiliza versiones sin azúcar de golosinas clásicas como galletas. Cantidad:8", "6.00 EUR", "https://cdn.cheapism.com/images/101017_diabetic_dessert_recipes_slide_7_fs.max-784x410.jpg");
+INSERT INTO products(title, description, price, image) values ("Pudin de chía y banana", "Solo un toque de miel endulza este postre rico en proteínas que promueve el azúcar en la sangre constante al tiempo.", "2.50 EUR", "https://cdn.cheapism.com/images/101017_diabetic_dessert_recipes_slide_4_fs.max-784x410.jpg");
